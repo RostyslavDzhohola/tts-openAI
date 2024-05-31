7e779f691abc968ad20c1734d1b2531de13b4a19
Rostyslav <dg1h1t@protonmail.com>
2024-05-30 23:51:49 -0500

    building server

---
+++ server/server.js
@@ -0,0 +1,6 @@
+// server.js
+// this is the server that will send api request to openai api and return an audio file
+
+//for testing lets output file into a folder using fs
+const express = require('express');
+const body
