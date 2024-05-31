950249dde9231795b2312969ccb2dd0979d1d445
Rostyslav <dg1h1t@protonmail.com>
2024-05-31 00:44:47 -0500

    server is working

--- server/server.js
+++ server/server.js
@@ -2,5 +2,39 @@
 // this is the server that will send api request to openai api and return an audio file
 
 //for testing lets output file into a folder using fs
+// server.js
 const express = require('express');
-const body
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const OpenAI = require('openai');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json()); // 
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.post('/convert', async (req, res) => {
+  const { text } = req.body;
+  console.log('Received text:', text);
+
+  const status = 200;
+  const body = {message: 'Text received and converted', text: text};
+
+  res.status(status).json(body);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  console.log('Request headers:', req.headers);
+  console.log('Request body:', req.body);
+  next();
+});
