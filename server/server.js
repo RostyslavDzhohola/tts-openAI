82fad7d2e126ad4bbde968ed8e8868a7b66500f2
Rostyslav <dg1h1t@protonmail.com>
2024-05-31 01:00:26 -0500

    the tts on server side working great

--- server/server.js
+++ server/server.js
@@ -3,29 +3,44 @@
 
 //for testing lets output file into a folder using fs
 // server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const OpenAI = require('openai');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const OpenAI = require("openai");
+const fs = require("fs");
+const path = require("path");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json()); // 
+app.use(bodyParser.json()); //
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post('/convert', async (req, res) => {
+const speechFile = path.resolve("./audio/speech.mp3");
+
+app.post("/convert", async (req, res) => {
   const { text } = req.body;
-  console.log('Received text:', text);
+  console.log("Received text:", text);
 
-  const status = 200;
-  const body = {message: 'Text received and converted', text: text};
+  try {
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: "nova",
+      input: text,
+    });
+    console.log(speechFile);
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFile, buffer);
 
-  res.status(status).json(body);
+    res.send("Text converted and saved to speech.mp3");
+  } catch (error) {
+    console.error("Error converting text:", error);
+    res.status(500).send("An error occurred while converting text");
+  }
 });
 
 app.listen(PORT, () => {
@@ -34,7 +49,7 @@ app.listen(PORT, () => {
 
 app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`);
-  console.log('Request headers:', req.headers);
-  console.log('Request body:', req.body);
+  console.log("Request headers:", req.headers);
+  console.log("Request body:", req.body);
   next();
 });
